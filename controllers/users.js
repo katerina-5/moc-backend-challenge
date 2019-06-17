3fb9dc8c20fb4a7b182b16ebcfddf81b765a743c
katerina-5 <pochta13524@gmail.com>
2019-06-18 00:04:17 +0300

    Add functions for get users list and detail information for user

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,30 @@ module.exports = {
     user_detail
 }
 
-function user_list(req, res, next) {
+async function user_list(request, response, next) {
     console.log('List of users');
+
+    try {
+        const result = await pool.query('select user_id, user_name from users');
+
+        response.status(200).json(result.rows);
+    } catch (error) {
+        console.log(error.message);
+        next(error);
+    }
 }
 
-function user_detail(req, res, next) {
+async function user_detail(request, response, next) {
     console.log('Detail information about user');
+
+    const user_id = request.params.id;
+
+    try {
+        const result = await pool.query('select user_id, user_name from users where user_id = $1', [user_id]);
+
+        response.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+        next(error);
+    }
 }
