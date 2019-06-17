bdbd1f2ee205dc9da4d4736b2796cd3f8036f2cb
katerina-5 <pochta13524@gmail.com>
2019-06-18 02:30:46 +0300

    Add function for send messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -4,6 +4,23 @@ module.exports = {
     message_create
 }
 
-function message_create(req, res, next) {
+async function message_create(request, response, next) {
     console.log('Message create');
+
+    const { user_id, room_id, message } = request.body;
+    const sent_date = Date(Date.now()).substring(0, 24);
+
+    try {
+        const result = await pool.query('insert into messages(sent_date, user_id, room_id, message) values($1, $2, $3, $4)',
+            [sent_date, user_id, room_id, message]);
+
+        let jsonResult = {
+            result: "message successfully sent"
+        }
+
+        response.status(200).json(jsonResult);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 }
